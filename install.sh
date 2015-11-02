6e9affc40ffdec27eb0793c0fce05d7729faac21
timlyo <timlyomaddison@gmail.com>
2015-11-02 11:39:02 +0000

    added mkdir stage to install

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
+echo "Making directories"
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3status
+
 echo "Installing i3 files"
 cp i3/config ~/.config/i3/config
+cp i3status/config ~/.config/i3status/config
