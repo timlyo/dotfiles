a00f540b7f2560afdc44dd8b5f6d238e9aaee1bd
Tim Maddison <timmaddison@qinetiq.com>
2016-01-18 11:41:06 +0000

    Added fish config

---
+++ fish/config.fish
@@ -0,0 +1,1 @@
+set -gx PATH ~/.gem/ruby/2.3.0/bin/ $PATH
--- install.sh
+++ install.sh
@@ -10,8 +10,11 @@ cp i3status/config ~/.config/i3status/config
 echo "Installing bashrc"
 cp .bashrc ~/.bashrc
 
+echo "Installing config.fish"
+cp fish/config.fish ~/.config/fish/config.fish
+
 echo "Installing i3lock files"
 # sudo yaourt -S i3lock-color-git
-sudo cp i3lock-fancy/lock /usr/local/bin
+sudo cp i3lock-fancy/lock /usr/local/bin/lockScreen
 sudo cp i3lock-fancy/lock.png /usr/local/bin
 sudo cp i3lock-fancy/lockdark.png /usr/local/bin
